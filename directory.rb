84ab1c15bfd43204a4fb7abade5837a37cddc4ce
James Maclachlan <james.maclachlan@meetupcall.com>
2020-01-28 15:52:41 +0000

    User input (through gets.chomp) to list students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# and then print them
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp
+  end
+
+  students
+end
+
 def print_header
-  puts "The students of Villain Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
@@ -24,10 +28,12 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
-# nothing happens until we call the methods
+
+students = input_students
+
 print_header
 print(students)
 print_footer(students)
