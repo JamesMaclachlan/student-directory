7d9be9c966d0bb7979257350ce2200d75a078e2a
James Maclachlan <james.maclachlan@meetupcall.com>
2020-01-30 11:35:21 +0000

    Only returning names begin with 'b' and have less than 12 letters. 'Select then do' rather than 'do then select' so indexes start at 1 and go up

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.select{|student| student[:name].start_with?('b')}.each.with_index(1) do |student, index|
-      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.select{|student| student[:name].length < 12 && student[:name].start_with?('b')}.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
