407ee1b491931c9eeda6868688e48ae900aa7ba1
James Maclachlan <james.maclachlan@meetupcall.com>
2020-02-07 16:38:55 +0000

    grouped by cohorts, no return if students.count = 0

--- directory.rb
+++ directory.rb
@@ -1,26 +1,38 @@
 def input_students
-  puts "Please enter the names of the students. On the following lines, please enter their: favourite hobby, height and city of birth"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and, on the following line, the months in which they enrolled. If you do not input a month of enrollment we will assume one. On the following lines please enter their: favourite hobby, height and city of birth."
+  puts "To finish, just hit return twice."
 
   students = []
 
   name = gets.chomp.capitalize
-  hobby = gets.chomp
-  height = gets.chomp
-  cob = gets.chomp
+  cohort = gets.chomp.capitalize
+  if cohort.empty?
+    cohort = "November"
+  end
+  hobby = gets.chomp.capitalize
+  height = gets.chomp.capitalize
+  cob = gets.chomp.capitalize
 
   while !name.empty? do
 
-    students << {name: name, cohort: :november, hobby: hobby, height: height, cob: cob}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort, hobby: hobby, height: height, cob: cob}
+      if students.count == 1
+        puts "Now we have #{students.count} student."
+      else
+        puts "Now we have #{students.count} students."
+      end
 
-    name = gets.chomp
+    name = gets.chomp.capitalize
     break if name.empty?
-    hobby = gets.chomp
-    height = gets.chomp
-    cob = gets.chomp
-  end
+    cohort = gets.chomp.capitalize
+    if cohort.empty?
+      cohort = "November"
+    end
+    hobby = gets.chomp.capitalize
+    height = gets.chomp.capitalize
+    cob = gets.chomp.capitalize
 
+  end
   students
 end
 
@@ -30,17 +42,25 @@ def print_header
 end
 
 def print(students)
-  students.select{|student| student[:name].start_with?('B') && student[:name].length < 12}.each.with_index(1) do |student, index|
+  students.select{|student| student[:name].start_with?('B') && student[:name].length < 12}.sort_by{|student| student[:cohort]}.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort, #{student[:hobby]}, #{student[:height]}, #{student[:cob]})".center(80)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(80)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great students.".center(80)
+  elsif students.count == 0
+    puts "There are currently #{students.count} students enrolled at Villains Academy".center(80)
+  else
+    puts "Overall, we have #{students.count} great students.".center(80)
+  end
 end
 
 students = input_students
 
-print_header
-print(students)
-print_footer(students)
+if students.count > 0
+  print_header
+  print(students)
+  print_footer(students)
+end
